0b520ced444b42a4a7d5824bca421fa0dd2f0a42
KKrenz <KKrenz@users.noreply.github.com>
2015-03-05 23:50:42 +0100

    Lösche überflüssigen answer_pair controller

--- app/controllers/answer_pair_controller.rb
+++
@@ -1,7 +0,0 @@
-class AnswerPairController < ApplicationController
-	def create
-  		@question = Question.find(params[:question_id])
-  		@answer_pair = @question.answer_pairs.create!(params[:answer_pair])
-  		redirect_to @question, :notice => "Comentario criado!"
-	end
-end
--- app/helpers/answer_pair_helper.rb
+++
@@ -1,2 +0,0 @@
-module AnswerPairHelper
-end
